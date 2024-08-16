d6c876f181135faea7d80321d5fe36f59e8931ed
antonio-bit-1109 <143196020+antonio-bit-1109@users.noreply.github.com>
2024-08-16 16:03:44 +0200

    gg

--- src/_Presentation/routes/UserRoute.ts
+++ src/_Presentation/routes/UserRoute.ts
@@ -8,6 +8,8 @@ router.route("/").post(UserController.createUser);
 router.route("/").get(UserController.getAllUsers);
 router.route("/edit").post(UserController.editUser);
 router.route("/status").post(UserController.changeStatus);
+//
+
 // router.route("/")
 // router.route("/").get(userController.GetAllUsers);
 // router.route("/").post(userController.CreateNewUser);
